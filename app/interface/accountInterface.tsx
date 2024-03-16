5e19954911d68408dfe3d3ddee3cd0e9acbcd0c2
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-03-16 23:48:46 +0300

    feat: fetch account list

---
+++ app/interface/accountInterface.tsx
@@ -0,0 +1,9 @@
+export interface AccountInterface {
+    id: string ,
+    firstName:string,
+    lastName:string,
+    birthDate: string,
+    loanAuthorization: boolean,
+    monthlyPay: number,
+    accountRef:string
+}
