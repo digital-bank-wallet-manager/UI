a1f4e32426daa35145496036e4ad01bf249b0971
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-03-22 12:18:21 +0300

    fix: remove accountId from provisioning form ;

--- app/components/accountComponets/accountDetails.tsx
+++ app/components/accountComponets/accountDetails.tsx
@@ -11,7 +11,7 @@ export interface AccountDetailsProps {
 }
 
 const AccountDetails: React.FC<AccountDetailsProps> = ({ account, balance }) => {
-
+    const accountId = account.id
     const [showFormProvisioning, setShowFormProvisioning] = useState(false);
 
     const handleBackButton = () => {
@@ -29,22 +29,21 @@ const AccountDetails: React.FC<AccountDetailsProps> = ({ account, balance }) => 
         <div>
             {
                 showFormProvisioning && (
-                    <Provisioning showFormProvisioning={showFormProvisioning} setShowFormProvisioning={setShowFormProvisioning}></Provisioning>
+                    <Provisioning formProvisioning={{ showFormProvisioning, setShowFormProvisioning }} account={account}></Provisioning>
                 )
             }
             <div className="flex flex-col gap-10 bg-white rounded-xl py-10" style={{ width: '60vw' }}>
                 <h1 className="text-2xl border-solid border-slat-200 border-b-8 w-full pl-5 ">Account Details</h1>
                 <div className=" flex flex-col gap-2 text-lg px-10">
-                    <p>Account ID: {account.id}</p>
                     <p>First Name: {account.firstName}</p>
                     <p>Last Name: {account.lastName}</p>
                     <p>Date of birth: {account.birthdate}</p>
                     <p>Monthly Pay: {account.monthlyPay} MGA</p>
                     <p>Account Reference: {account.accountRef}</p>
                     <p>Actual Balance: {balance.amount} MGA  at {actualDate}</p>
-                    <button type="button" onClick={handleFormProvisioning} className="border-solid border-red-600 bg-red-600 border-2 rounded py-2  hover:bg-red-700 hover:border-red-700  transition duration-200 text-white w-52 mt-3"> 
-                        provide your balance
-                    </button>
+                        <button type="button" onClick={handleFormProvisioning} className="border-solid border-red-600 bg-red-600 border-2 rounded py-2  hover:bg-red-700 hover:border-red-700  transition duration-200 text-white w-52 mt-3"> 
+                            provide your balance
+                        </button>
                     <button type="button" className="self-end text-xl text-black duration-75 hover:scale-110 flex flex-row items-center" onClick={handleBackButton}><IoChevronBackSharp /> Back</button>
                 </div>
             </div>
